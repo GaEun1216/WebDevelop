580d6ae766c270cb775ec58740928fe7537c6cba
GaEun1216 <gani6382@gmail.com>
2024-04-18 19:44:54 +0900

    보안강화

---
+++ key.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAf8i7uuYEqOusCfsnjCsvX2MT-ewq2tTM",
+    authDomain: "test-921f8.firebaseapp.com",
+    databaseURL: "https://test-921f8-default-rtdb.firebaseio.com",
+    projectId: "test-921f8",
+    storageBucket: "test-921f8.appspot.com",
+    messagingSenderId: "320684433449",
+    appId: "1:320684433449:web:7207e85a411cc42af57c38",
+    measurementId: "G-DM95L40522"
+};
